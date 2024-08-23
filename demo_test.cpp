35eb83914bce8cb75ab0537d643ad88969c3500d
Thefcraft <76678136+thefcraft@users.noreply.github.com>
2024-08-23 19:57:32 +0530

    Create demo_test.cpp

---
+++ demo_test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "test.assert.h"
+using namespace std;
+int main(){
+    // RUN_TESTS;
+    RUN_TESTS;
+    return 0;
+}
+
+MAKE_TESTS{
+    It(divide by zero){
+        vector<int> a = {1,2,3,4,5};
+        vector<int> b = {1,2,23,23,23};
+        asserteq(a, b);
+    }
+    It(mul by zero){
+        asserteq("test", "test");
+    }
+    It(randome testcase){
+        asserteq("test", "test");
+    }
+    {
+        asserteq("test", "test");   
+    }
+    {
+        assert((bool)72);
+    }
+    {
+        assert(false);
+    }
+}
