9bc4852b004ad0154bc6e0d60558bde868928c69
Thefcraft <76678136+thefcraft@users.noreply.github.com>
2024-12-18 17:41:49 +0530

    add metadata

--- webpage/app/layout.tsx
+++ webpage/app/layout.tsx
@@ -12,9 +12,39 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Fmt-Display-Cpp",
-  description: "A lightweight C++ library for custom formatted output using templates, featuring type-based printing and support for standard and custom types.",
+const url = 'https://fmt-display-cpp.thefcraft.site'
+export const metadata = {
+  title: "fmt-display-cpp - Modern C++ Formatting Library",
+  description:
+    "fmt-display-cpp is a modern C++ library for enhanced text formatting and custom display traits. Simplify your console output with ANSI styling, secure formatting, and intuitive tools.",
+  authors: [{ name: "ThefCraft" }],
+  keywords:
+    "C++, text formatting, custom display traits, ANSI styling, C++ library, console applications, CLI tools, debugging, output customization",
+  openGraph: {
+    title: "fmt-display-cpp - Modern C++ Formatting Library",
+    description:
+      "A powerful C++ library for custom text formatting, ANSI styling, and intuitive output tools. Simplify and enhance your console applications.",
+    url: "https://github.com/thefcraft/fmt-display-cpp",
+    type: "website",
+    images: [
+      {
+        url: `${url}/image.png`,
+        width: 1200,
+        height: 630,
+        alt: "fmt-display-cpp banner",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "fmt-display-cpp - Modern C++ Formatting Library",
+    description:
+      "Simplify console output in C++ with custom display traits, ANSI styling, and secure formatted output.",
+    images: [`${url}/image.png`],
+  },
+  icons: {
+    icon: `${url}/favicon.ico`,
+  },
 };
 
 export default function RootLayout({
